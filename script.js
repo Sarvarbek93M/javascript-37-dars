e6ed3ad14bf7d05434fb8ee5a43931f35f349ebc
Sarvarbek <hamkorbank.sms@gmail.com>
2022-05-13 14:10:36 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const startDate = new Date();
+// console.log(date);
+// //------------ get orqali malumotlarni olinadi
+// console.log(date.getHours());
+// //----------------getUTCHours() gmtga nisbatan farqini kursatadi
+// console.log(date.getUTCHours());
+// //---------------------getTimezoneOffset esa gmtga nisbatan "-" da minutda kursatadi
+// console.log(date.getTimezoneOffset());
+
+//---------------- set orqali malumot kiritamiza
+// console.log(date.setHours(11));
+// console.log(date);
+for (let i = 0; i > 100000; i++) {
+  let a = 1 ** 3;
+}
+
+const endDate = new Date();
+alert(tsikl ${endDate-startDate} millisekund ishladi);
